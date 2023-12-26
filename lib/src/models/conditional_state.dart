24a03ebfdcc88689b19e17ab4e82ba847ea8cd50
lamnhan066 <hello@lamnhan.dev>
2023-12-26 09:44:21 +0700

    Marks `ConditionalState.dontSatisfyWithMinCalls`, `ConditionalState.dontSatisfyWithMinDays` and `ConditionalState.dontSatisfyWithMinCallsAndDays` as deprecated with `ConditionalState.notSatisfiedWithMinCalls`, `ConditionalState.notSatisfiedWithMinDays` and `ConditionalState.notSatisfiedWithMinCallsAndDays`.

--- lib/src/models/conditional_state.dart
+++ lib/src/models/conditional_state.dart
@@ -1,26 +1,40 @@
 /// Result of the review request.
 enum ConditionalState {
-  /// The conditions have been satisfied but the `keepRemind` was disabled.
+  /// The conditions have been satisfied but the `keepRemind` has been disabled.
   keepRemindDisabled(
-      'The conditions have been satisfied but the `keepRemind` was disabled'),
+      'The conditions have been satisfied but the `keepRemind` has been disabled'),
 
-  /// This version is satisfied with `noRequestVersions` => Don't satisfied.
+  /// This version is satisfied with `noRequestVersions` => Not satisfied.
   noRequestVersion(
-      'This version is satisfied with `noRequestVersions` => Don\'t satisfied'),
+      'This version is satisfied with `noRequestVersions` => Not satisfied'),
 
-  /// Don't satisfy with minCalls condition.
-  dontSatisfyWithMinCalls('Don\'t satisfy with minCalls condition'),
+  /// Not satisfied with minCalls condition.
+  @Deprecated('Use `ConditionalState.notSatisfiedWithMinCalls instead`')
+  dontSatisfyWithMinCalls('Not satisfied with minCalls condition'),
 
-  /// Don't satisfy with minDays condition.
-  dontSatisfyWithMinDays('Don\'t satisfy with minDays condition'),
+  /// Not satisfied with minCalls condition.
+  notSatisfiedWithMinCalls('Not satisfied with minCalls condition'),
 
-  /// Don't satisfy with minCalls and minDays.
-  dontSatisfyWithMinCallsAndDays("Don't satisfy with minCalls and minDays"),
+  /// Not satisfied with minDays condition.
+  @Deprecated('Use `ConditionalState.notSatisfiedWithMinDays instead`')
+  dontSatisfyWithMinDays('Not satisfied with minDays condition'),
+
+  /// Not satisfied with minDays condition.
+  notSatisfiedWithMinDays('Not satisfied with minDays condition'),
+
+  /// Not satisfied with minCalls and minDays.
+  @Deprecated('Use `ConditionalState.notSatisfiedWithMinCallsAndDays instead`')
+  dontSatisfyWithMinCallsAndDays("Not satisfied with minCalls and minDays"),
+
+  /// Not satisfied with minCalls and minDays.
+  notSatisfiedWithMinCallsAndDays("Not satisfied with minCalls and minDays"),
 
   /// Satisfied with all conditions.
   satisfied('Satisfied with all conditions');
 
-  /// Describe the conditional state.
+  /// Describes the conditional state.
+  ///
+  /// Useful for printing logs.
   final String text;
 
   /// Result of the review request..
